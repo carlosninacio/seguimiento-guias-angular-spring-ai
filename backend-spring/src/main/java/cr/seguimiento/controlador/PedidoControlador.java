8b5cd99f2539426a7e3e6bdb0137bae5f0a9ba4d
carlosninacio <cigramirez@poligran.edu.co>
2025-10-29 15:52:57 -0500

    Se creó la petición para listar los pedidos

--- backend-spring/src/main/java/cr/seguimiento/controlador/PedidoControlador.java
+++ backend-spring/src/main/java/cr/seguimiento/controlador/PedidoControlador.java
@@ -1,12 +1,16 @@
 package cr.seguimiento.controlador;
 
+import cr.seguimiento.modelo.Pedido;
 import cr.seguimiento.servicio.PedidoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("seguimiento-app") // http://localhost:8080/seguimiento-app - Puerto aplicacion por default
@@ -17,4 +21,13 @@ public class PedidoControlador {
 
     @Autowired
     private PedidoService pedidoServicio;
+
+    @GetMapping("/pedidos") // Configuración para recibir peticiones
+    // Peticiones -> http://localhost:8080/seguimiento-app/pedidos
+    public List<Pedido> obtenerPedidos() {
+        List<Pedido> pedidos = this.pedidoServicio.listarPedidos();
+        logger.info("Productos obtenidos: ");
+        pedidos.forEach(pedido -> logger.info(pedido.toString()));
+        return pedidos;
+    }
 }
