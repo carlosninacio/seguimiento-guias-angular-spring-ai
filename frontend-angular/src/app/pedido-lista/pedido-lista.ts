184b92e28e0d187a417876d095c9b0cdb67d09eb
carlosninacio <cigramirez@poligran.edu.co>
2025-10-29 20:10:38 -0500

    Se crea el componente pedido-lista

---
+++ frontend-angular/src/app/pedido-lista/pedido-lista.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pedido-lista',
+  imports: [],
+  templateUrl: './pedido-lista.html',
+  styleUrl: './pedido-lista.css',
+})
+export class PedidoLista {
+
+}
