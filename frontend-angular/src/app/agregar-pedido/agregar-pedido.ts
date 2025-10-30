9aa59ebdf04ac5319bb13c2a0a11ad7f2d80283e
carlosninacio <cigramirez@poligran.edu.co>
2025-10-30 13:56:50 -0500

    Se crea el componente agregar pedido, se crea su ruta y se agrega a la navbar

---
+++ frontend-angular/src/app/agregar-pedido/agregar-pedido.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-pedido',
+  imports: [],
+  templateUrl: './agregar-pedido.html'
+})
+export class AgregarPedido {
+
+}
--- frontend-angular/src/app/app.routes.ts
+++ frontend-angular/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { PedidoLista } from './pedido-lista/pedido-lista';
+import { AgregarPedido } from './agregar-pedido/agregar-pedido';
 
 export const routes: Routes = [
     {path: 'pedidos', component: PedidoLista},
-    {path: '', redirectTo:'pedidos', pathMatch: 'full'}
+    {path: '', redirectTo:'pedidos', pathMatch: 'full'},
+    {path: 'agregar-pedido', component: AgregarPedido}
 ];
